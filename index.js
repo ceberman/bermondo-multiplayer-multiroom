7cab78ec3a35e937348d0ec6fa67aad6c5115f8b
Charles Berman <ceberman@gmail.com>
2018-09-21 23:20:57 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,338 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+var debugLevel = 10;
+
+server.listen(port, function () {
+              console.log('Server listening at port %d', port);
+              });
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+// ceb
+// usernames which are currently connected to the chat
+// var usernames = {};
+// var numUsers = 0;
+allRoomUserNames = {};
+allRoomUserCounts = {};
+
+// ceb
+// new way of doing command history
+// right now there's only one room in allRoomCommands
+var defaultRoom = 'waitingRoom';
+var allRoomCommands = {};
+createRoomDataStructures(defaultRoom);
+
+// ceb
+// rooms subsystem
+var rooms = [ defaultRoom , 'room2', 'room3' ];
+
+// ceb - read in menu system
+var fs = require('fs');
+var  allMenus;
+fs.readFile('webmenus.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  allMenus = JSON.parse(data);
+});
+
+
+io.on('connection', function (socket) {
+      var addedUser = false;
+
+      // when the client emits 'new message', this listens and executes
+      socket.on('new message', function (data) {
+                // we tell the client to execute 'new message'
+                socket.broadcast.to(socket.room).emit('new message', {
+                                      username: socket.username,
+                                      message: data
+                                      });
+                });
+
+      // ceb
+      // when the client emits 'new message', this listens and executes
+      socket.on('RemoteMenu', function (data) {
+                // we tell the client to execute 'new message'
+                socket.broadcast.to(socket.room).emit('RemoteMenu', {
+                                      username: socket.username,
+                                      message: data
+                                      });
+
+
+                // ceb unfinished
+                // need to unpack data and figure out command name and command values
+                // logCommand(defaultRoom, );
+                logMenuCommand(socket.room, data);
+                if (debugLevel > 0) {
+                  console.log('new menuCommand: ');
+                  consolePrintMenuCommandObject(data);
+                  consolePrintCommandDictionary(socket.room);
+                }
+                });
+
+      // ceb
+      socket.on('Player Position', function (data) {
+                // we tell the client to execute 'new message'
+                socket.broadcast.to(socket.room).emit('Player Position', {
+                                      username: socket.username,
+                                      message: data
+                                      });
+
+                });
+
+      // when the client emits 'add user', this listens and executes
+      socket.on('add user', function (username) {
+                // we store the username in the socket session for this client
+                socket.username = username;
+                // multiroom additions
+                socket.room = defaultRoom;
+                socket.join(defaultRoom);
+
+                // add the client's username to the global list
+                userEntersRoomDataStructures(defaultRoom, username)
+                addedUser = true;
+
+                socket.emit('login', {
+                            numUsers: allRoomUserCounts[defaultRoom]
+                            });
+
+                // used only for the HTML client
+                socket.emit('set menus', allMenus);
+
+                // ceb
+                emitCatchup(socket, defaultRoom);
+
+                //
+                socket.emit('new message', {
+                  username: 'SERVER',
+                  message: 'you have connected to waitingRoom'});
+
+
+                // echo globally (all clients) that a person has connected
+                socket.broadcast.to(defaultRoom).emit('user joined', {
+                                      username: socket.username,
+                                      numUsers: allRoomUserCounts[defaultRoom]
+                                      });
+
+                socket.emit('updaterooms', rooms, defaultRoom);
+
+                // ceb
+                // for Unity/C# applications, need a well formed object with keys
+                // Unity (by default at least) does not support .NET 4
+                // and the assemblies as noted in https://stackoverflow.com/questions/3142495/deserialize-json-into-c-sharp-dynamic-object
+                //  using System.Web.Script.Serialization;
+                // or
+                //  using System.Dynamic;
+                //  using System.Web.Script.Serialization;
+                //
+                var roomDataStruct = {roomArray : rooms, currentRoom : defaultRoom};
+                socket.emit('updateroomsJson', roomDataStruct);
+
+
+                });
+
+        socket.on('switchRoom', function(newroom){
+                  // sent message to OLD room
+                  socket.broadcast.to(socket.room).emit('new message', 'SERVER', socket.username+' has left this room');
+                  // echo globally that this client has left
+                  socket.broadcast.to(socket.room).emit('user left', {
+                                        username: socket.username,
+                                        numUsers: allRoomUserCounts[socket.room]
+                                        });
+
+              		socket.leave(socket.room);
+                  userLeavesRoomDataStructures(socket.room, socket.username);
+              		socket.join(newroom);
+              		socket.emit('new message', {
+                    username: 'SERVER',
+                    message: 'you have connected to '+ newroom});
+
+
+
+              		// update socket session room title
+              		socket.room = newroom;
+              		socket.broadcast.to(newroom).emit('new message', {
+                    username: 'SERVER',
+                    message: socket.username+' has joined this room'});
+              		socket.emit('updaterooms', rooms, newroom);
+
+                  // bermondo specific
+                  var roomDataStruct = {roomArray : rooms, currentRoom : newroom};
+                  socket.emit('updateroomsJson', roomDataStruct);
+                  userEntersRoomDataStructures(newroom, socket.username)
+                  emitCatchup(socket, newroom);
+
+              	});
+
+      // when the client emits 'typing', we broadcast it to others
+      socket.on('typing', function () {
+                socket.broadcast.to(socket.room).emit('typing', {
+                                      username: socket.username
+                                      });
+                });
+
+      // when the client emits 'stop typing', we broadcast it to others
+      socket.on('stop typing', function () {
+                socket.broadcast.to(socket.room).emit('stop typing', {
+                                      username: socket.username
+                                      });
+                });
+
+      // when the user disconnects.. perform this
+      socket.on('disconnect', function () {
+
+            if (addedUser) {
+            // if (usernames.hasOwnProperty(socket.username)) {
+                  // remove the username from global usernames list
+                  userLeavesRoomDataStructures(socket.room, socket.username);
+                  /*
+                  var usernames = allRoomUserNames[socket.room];
+                  delete usernames[socket.username];
+                  allRoomUserCounts[socket.room] = allRoomUserCounts[socket.room] - 1;
+
+                  // ceb
+                  if (allRoomUserCounts[socket.room] <= 0) {
+                    emptyRoomDataStructures(socket.room);
+                  }
+                  */
+
+                  // echo globally that this client has left
+                  socket.broadcast.to(socket.room).emit('user left', {
+                                        username: socket.username,
+                                        numUsers: allRoomUserCounts[socket.room]
+                                        });
+
+            } else {
+              // this seems to happen if client disconnects before giving logging in
+              console.log("Warning: received disconnect for unknown user %s", socket.username);
+            }
+
+        });
+
+      });
+
+// ceb
+
+// handling user enter and leave
+function userLeavesRoomDataStructures(roomName, userName)
+{
+  var usernames = allRoomUserNames[roomName];
+  if (usernames == undefined) {
+    // shouldn't happen
+    console.log("userLeavesDataStructure(%s, %s), no usernames", roomName, userName);
+    return;
+  }
+  delete usernames[userName];
+  if (allRoomUserCounts[roomName] == undefined) {
+    // shouldn't happen
+    console.log("userLeavesDataStructure(%s, %s), no allRoomUserCounts[%s]", roomName, userName, roomName);
+    return;
+  }
+  allRoomUserCounts[roomName] = allRoomUserCounts[roomName] - 1;
+
+  // ceb
+  if (allRoomUserCounts[roomName] <= 0) {
+    emptyRoomDataStructures(roomName);
+  }
+}
+
+function userEntersRoomDataStructures(roomName, userName)
+{
+  confirmOrCreateRoomDataStructures(roomName);
+  var usernames = allRoomUserNames[roomName];
+  usernames[userName] = userName;
+  allRoomUserCounts[roomName] = allRoomUserCounts[roomName] + 1;
+}
+// handling room creation and deletion
+function confirmOrCreateRoomDataStructures(roomName) {
+  if (allRoomCommands.hasOwnProperty(roomName)) {
+    if (debugLevel > 0) {
+      console.log("confirmOrCreateRoomDataStructures(%s): already exists", roomName);
+    }
+  } else {
+    createRoomDataStructures(roomName);
+    if (debugLevel > 0) {
+      console.log("confirmOrCreateRoomDataStructures(%s): NEED TO CREATE", roomName);
+    }
+  }
+}
+function createRoomDataStructures(roomName) {
+  if (debugLevel > 0) {
+    console.log("createRoomDataStructures(%s)", roomName);
+  }
+  var newRoomCommandDictionary = {};
+  allRoomCommands[roomName] = newRoomCommandDictionary;
+  var newRoomUserNames = {};
+  allRoomUserNames[roomName] = newRoomUserNames;
+  allRoomUserCounts[roomName] = 0;
+}
+
+function deleteRoomDataStructures(roomName) {
+  if (debugLevel > 0) {
+    console.log("deleteRoomDataStructures(%s)", roomName);
+  }
+  delete allRoomCommands[roomName];
+}
+
+function emptyRoomDataStructures(roomName) {
+  if (debugLevel > 0) {
+    console.log("emptyRoomDataStructures(%s)", roomName);
+  }
+  var commandDictionary = allRoomCommands[roomName];
+  for (var commandName in commandDictionary) {
+    if (commandDictionary.hasOwnProperty(commandName)) {
+      delete(commandDictionary[commandName]);
+    }
+  }
+  // shouldn't have to remove userNames or zero out allRoomUserCounts
+  // might want to verify this with an assertion
+}
+
+function logMenuCommand(roomName, commandObject) {
+  // this is necessary if the server restarts, but clients have previously connected
+  confirmOrCreateRoomDataStructures(roomName);
+
+  var roomCommandDictionary = allRoomCommands[roomName];
+  var commandName = commandObject['menuName'];
+  roomCommandDictionary[commandName] = commandObject;
+}
+
+function consolePrintCommandDictionary(roomName)
+{
+  var commandDictionary = allRoomCommands[roomName];
+  console.log("room: %s.commandDictionary ------->", roomName);
+  for (var commandName in commandDictionary) {
+    if (commandDictionary.hasOwnProperty(commandName)) {
+      var commandObject = commandDictionary[commandName];
+      consolePrintMenuCommandObject(commandObject);
+    }
+  }
+}
+
+function emitCatchup(socket, roomName)
+{
+  var commandDictionary = allRoomCommands[roomName];
+  if (debugLevel > 0) {
+    console.log('room: %s.emitCatchup: commandDictionary ------->', roomName);
+    consolePrintCommandDictionary(roomName);
+  }
+  for (var commandName in commandDictionary) {
+    if (commandDictionary.hasOwnProperty(commandName)) {
+      var commandObject = commandDictionary[commandName];
+      socket.emit('RemoteMenu', {
+        username: 'SERVER',
+        message: commandObject});
+    }
+  }
+}
+
+function consolePrintMenuCommandObject(singleMenuObject)
+{
+  console.log('===>menuName = %s, menuItem = %s', singleMenuObject['menuName'], singleMenuObject['menuItem']);
+}
---
+++ public/main.js
@@ -0,0 +1,511 @@
+// ceb placing these into global scope
+// ... appears to work for switchroom,
+//     but haven't put the time into making this more consistent
+
+var socket;
+function switchRoom(room){
+  socket.emit('switchRoom', room);
+}
+
+// --------------start of onload-------------------------
+$(function() {
+  var FADE_TIME = 150; // ms
+  var TYPING_TIMER_LENGTH = 400; // ms
+  var COLORS = [
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+  ];
+
+  // Initialize varibles
+  var $window = $(window);
+  var $usernameInput = $('.usernameInput'); // Input for username
+  var $messages = $('.messages'); // Messages area
+  var $inputMessage = $('.inputMessage'); // Input message input box
+
+  var $loginPage = $('.login-div'); // The login page
+  var $chatPage = $('.chat.page'); // The chatroom page
+
+  // added by ceb
+  var $contentPageDiv = $('.all-other-content');
+  var $bandChatterDiv = $('.band-chatter-div');
+  $contentPageDiv.hide();
+  $(".dropdown-toggle").hide();
+
+  var welcomeMessage = "Welcome to Your Music Video";
+
+
+  var allMenus;
+
+  // ceb  up event handling
+
+  // for buttons may not need this!
+  $('#tonicButton').on('click', function(event) {
+    event.preventDefault(); // To prevent following the link (optional)
+    console.log ("tonicButton click");
+    });
+
+
+  // for HTML select objects (not sure if the best)
+  var menuItemVal = '';
+  var menuName = '';
+  var ccToggle=0;
+  $('select').click(function () {
+        ccToggle++;
+        if (ccToggle==2) {
+                    $(this).change();
+                    ccToggle = 0;
+        }
+
+    })
+  .change(function () {
+          menuItemVal = $(this).val();
+          menuName = this.id;            // not sure this is the right way
+          setServerMenuItem(menuName, menuItemVal);
+          ccToggle = 0;
+  });
+
+
+
+
+
+  // end of ceb adds
+
+
+  // Prompt for setting a username
+  var username;
+  var connected = false;
+  var typing = false;
+  var lastTypingTime;
+  var $currentInput = $usernameInput.focus();
+
+  socket = io();
+
+  //added by ceb
+  function getMenu(menuName) { return allMenus[menuName] }
+
+  function setServerMenuItem (menuName, menuItem) {
+    var majorMenuSubMenu;
+    var message = "";
+
+    majorMenuSubMenu = getMenu(menuName);
+    message = '{RemoteMenu,' + menuName + "," + menuItem + '}';
+    addChatMessage({
+                     username: username,
+                     message: message
+                     });
+
+    socket.emit('RemoteMenu', {
+                          menuName: menuName,
+                          menuItem: menuItem,
+                          arg1: "",
+                          arg2: "",
+                          arg3: "",
+                          arg4: "",
+                          arg5: ""
+                    });
+    checkMenuForPlayerPosition(menuName);
+  }
+
+  function checkMenuForPlayerPosition(menuName) {
+    if ((menuName === "Drum Preset") || (menuName === "Kit Type")) {
+        sendPlayerPosition(1.35, 2.5, 14.5, 0.0, 175.0, 0.0);
+    } else if (menuName === "Bass Pattern") {
+        sendPlayerPosition(4.1, 2.6, 14, 0.0, 0.0, 0.0);
+    } else if (menuName === "Piano") {
+        sendPlayerPosition(7.0, 2.6, 12.0, 0.0, 145.0, 0.0);
+    } else if (menuName === "Tonic") {
+        sendPlayerPosition(-7, 3.5, 5.5, 0.0, 60.0, 0.0);
+    } else if (menuName === "Scale") {
+        sendPlayerPosition(-8.5, 3.6, 11.2, 0.0, 0.0, 0.0);
+    }
+  }
+
+    function sendPlayerPosition (posX, posY, posZ, rotX, rotY, rotZ) {
+       socket.emit('Player Position' , {
+                          playerName: '',
+                          positionX: posX,
+                          positionY: posY,
+                          positionZ: posZ,
+                          rotationX: rotX,
+                          rotationY: rotY,
+                          rotationZ: rotZ
+                    });
+    }
+
+  function createMenusAndSubMenus () {
+      var
+      i=0,
+      subName,
+      optionMenuOrDiv,
+      majorMenuName,
+      majorMenuSubMenu;
+
+      for (var name in allMenus) {
+          if (allMenus.hasOwnProperty(name)) {
+              majorMenuName = name ;
+//             console.log ("majorMenuName: " + majorMenuName);
+              majorMenuSubMenu = getMenu(name);
+              optionMenuOrDiv = document.getElementById(majorMenuName);
+              if (optionMenuOrDiv != null) {
+                  for (i= 0; i < majorMenuSubMenu.length; i++ ) {
+                      subName = majorMenuSubMenu[i];
+//                      console.log ("subName: " + subName);
+                      createRadioButton(majorMenuName, optionMenuOrDiv, subName);
+
+                  }
+              }
+          }
+      }
+  }
+
+  function createSubMenu(docElement, subName){
+      var menuItem = document.createElement('option');
+      menuItem.setAttribute('value', subName);
+      menuItem.appendChild(document.createTextNode(subName));
+      docElement.appendChild(menuItem);
+
+  }
+
+  $('#radiobutton').on('click', function(event) {
+           var thisType = $(this).attr('type');
+   });
+
+  function createRadioButton(majorMenuName, parentDocElement, subName) {
+        var buttonName;
+        var subDiv = document.createElement('div');
+        var label = document.createElement('label');
+
+        subDiv.setAttribute('class', 'radio');
+        parentDocElement.appendChild(subDiv);
+        subDiv.appendChild(label);
+        var radioButton = document.createElement('input');
+
+        buttonName = majorMenuName + '-' + subName;
+        radioButton.setAttribute('type', 'radio');
+        radioButton.setAttribute('name', majorMenuName);
+        radioButton.setAttribute('id', buttonName);
+        radioButton.setAttribute('value', subName);
+        radioButton.addEventListener('click',
+            function() {
+             var thisType = $(this).attr('type');
+                // better be of type 'radio'
+                 if (thisType != 'radio') {
+                     console.log("bad event on type" + thisType)
+                 } else { // ok!!
+                      setServerMenuItem(this.name, this.value)
+                 }
+
+        });
+
+        label.appendChild(radioButton);
+        label.appendChild(document.createTextNode(subName));
+
+  }
+
+
+
+  function addParticipantsMessage (data) {
+    var message = '';
+    if (data.numUsers === 1) {
+      message += "there's 1 participant";
+    } else {
+      message += "there are " + data.numUsers + " participants";
+    }
+    log(message);
+  }
+
+
+  // Sets the client's username
+  function setUsername () {
+    username = cleanInput($usernameInput.val().trim());
+
+    // If the username is valid
+    if (username) {
+      $loginPage.fadeOut();
+
+      $chatPage.show(); //changed by ceb
+
+      $loginPage.off('click');
+      $currentInput = $inputMessage.focus();
+
+      // Tell the server your username
+      socket.emit('add user', username);
+    }
+  }
+
+  // Sends a chat message
+  function sendMessage () {
+    var message = $inputMessage.val();
+    // Prevent markup from being injected into the message
+    message = cleanInput(message);
+    // if there is a non-empty message and a socket connection
+    if (message && connected) {
+      $inputMessage.val('');
+      addChatMessage({
+        username: username,
+        message: message
+      });
+      // tell server to execute 'new message' and send along one parameter
+      socket.emit('new message', message);
+    }
+  }
+
+  // Log a message
+  function log (message, options) {
+    var $el = $('<li>').addClass('log').text(message);
+    addMessageElement($el, options);
+  }
+
+  // Adds the visual chat message to the message list
+  function addChatMessage (data, options) {
+    // Don't fade the message in if there is an 'X was typing'
+    var $typingMessages = getTypingMessages(data);
+    options = options || {};
+    if ($typingMessages.length !== 0) {
+      options.fade = false;
+      $typingMessages.remove();
+    }
+
+    var $usernameDiv = $('<span class="username"/>')
+      .text(data.username)
+      .css('color', getUsernameColor(data.username));
+    var $messageBodyDiv = $('<span class="messageBody">')
+      .text(data.message);
+
+    var typingClass = data.typing ? 'typing' : '';
+    var $messageDiv = $('<li class="message"/>')
+      .data('username', data.username)
+      .addClass(typingClass)
+      .append($usernameDiv, $messageBodyDiv);
+
+    addMessageElement($messageDiv, options);
+  }
+
+  // Adds an arbitrary string to the message list
+  function addArbitraryMessage (unameFromMessage, arbitraryString) {
+
+    addChatMessage({
+      username: unameFromMessage,
+      message: arbitraryString
+    });
+  }
+
+  // Adds the visual chat typing message
+  function addChatTyping (data) {
+    data.typing = true;
+    data.message = 'is typing';
+    addChatMessage(data);
+  }
+
+  // Removes the visual chat typing message
+  function removeChatTyping (data) {
+    getTypingMessages(data).fadeOut(function () {
+      $(this).remove();
+    });
+  }
+
+  // Adds a message element to the messages and scrolls to the bottom
+  // el - The element to add as a message
+  // options.fade - If the element should fade-in (default = true)
+  // options.prepend - If the element should prepend
+  //   all other messages (default = false)
+  function addMessageElement (el, options) {
+    var $el = $(el);
+
+    // Setup default options
+    if (!options) {
+      options = {};
+    }
+    if (typeof options.fade === 'undefined') {
+      options.fade = true;
+    }
+    if (typeof options.prepend === 'undefined') {
+      options.prepend = false;
+    }
+
+    // Apply options
+    if (options.fade) {
+      $el.hide().fadeIn(FADE_TIME);
+    }
+    if (options.prepend) {
+      $messages.prepend($el);
+    } else {
+      $messages.append($el);
+    }
+    $messages[0].scrollTop = $messages[0].scrollHeight;
+  }
+
+  // Prevents input from having injected markup
+  function cleanInput (input) {
+    return $('<div/>').text(input).text();
+  }
+
+  // Updates the typing event
+  function updateTyping () {
+    if (connected) {
+      if (!typing) {
+        typing = true;
+        socket.emit('typing');
+      }
+      lastTypingTime = (new Date()).getTime();
+
+      setTimeout(function () {
+        var typingTimer = (new Date()).getTime();
+        var timeDiff = typingTimer - lastTypingTime;
+        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+          socket.emit('stop typing');
+          typing = false;
+        }
+      }, TYPING_TIMER_LENGTH);
+    }
+  }
+
+  // Gets the 'X is typing' messages of a user
+  function getTypingMessages (data) {
+    return $('.typing.message').filter(function (i) {
+      return $(this).data('username') === data.username;
+    });
+  }
+
+  // Gets the color of a username through our hash function
+  function getUsernameColor (username) {
+    // Compute hash code
+    var hash = 7;
+    for (var i = 0; i < username.length; i++) {
+       hash = username.charCodeAt(i) + (hash << 5) - hash;
+    }
+    // Calculate color
+    var index = Math.abs(hash % COLORS.length);
+    return COLORS[index];
+  }
+
+
+
+  // Keyboard events
+
+  $window.keydown(function (event) {
+    // Auto-focus the current input when a key is typed
+    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+      $currentInput.focus();
+    }
+    // When the client hits ENTER on their keyboard
+    if (event.which === 13) {
+      if (username) {
+        sendMessage();
+        socket.emit('stop typing');
+        typing = false;
+      } else {
+        setUsername();
+      }
+    }
+  });
+
+  $inputMessage.on('input', function() {
+    updateTyping();
+  });
+
+  // Click events
+
+  // Focus input when clicking anywhere on login page
+  $loginPage.click(function () {
+    $currentInput.focus();
+  });
+
+  // Focus input when clicking on the message input's border
+  $inputMessage.click(function () {
+    $inputMessage.focus();
+  });
+
+  // Socket events
+
+  // Whenever the server emits 'login', log the login message
+  socket.on('login', function (data) {
+    connected = true;
+    // Display the welcome message
+    var message = welcomeMessage;
+    log(message, {
+      prepend: true
+    });
+    addParticipantsMessage(data);
+  });
+
+  socket.on('set menus', function (data) {
+    allMenus = data;
+    createMenusAndSubMenus();
+
+    // added by ceb
+    $chatPage.show(); //changed by ceb, not working
+    $('.dropdown-toggle').fadeIn();
+    $contentPageDiv.fadeIn();
+    $bandChatterDiv.fadeIn();
+
+
+  });
+
+  // Whenever the server emits 'new message', update the chat body
+  socket.on('new message', function (data) {
+    addChatMessage(data);
+  });
+
+  socket.on('RemoteMenu', function (data) {
+    // addChatMessage(data);
+
+    var userNameFromMessage = data.username;
+    var messageContents = data.message;
+    var menuNameFromMessage = messageContents.menuName;
+    var menuItemFromMessage = messageContents.menuItem;
+    var messageString = menuNameFromMessage + ': ' + menuItemFromMessage;
+    addArbitraryMessage(userNameFromMessage, messageString);
+
+  });
+
+  socket.on('Player Position', function (data) {
+    addChatMessage(data);
+  });
+
+
+  // Whenever the server emits 'user joined', log it in the chat body
+  socket.on('user joined', function (data) {
+    log(data.username + ' joined');
+    addParticipantsMessage(data);
+  });
+
+  // Whenever the server emits 'user left', log it in the chat body
+  socket.on('user left', function (data) {
+    log(data.username + ' left');
+    addParticipantsMessage(data);
+    removeChatTyping(data);
+  });
+
+  // Whenever the server emits 'typing', show the typing message
+  socket.on('typing', function (data) {
+    addChatTyping(data);
+  });
+
+  // Whenever the server emits 'stop typing', kill the typing message
+  socket.on('stop typing', function (data) {
+    removeChatTyping(data);
+  });
+
+  // for multiroom
+  // listener, whenever the server emits 'updaterooms', this updates the room the client is in
+	socket.on('updaterooms', function(rooms, current_room) {
+		$('#rooms').empty();
+		$.each(rooms, function(key, value) {
+			if(value == current_room){
+				$('#rooms').append('<div>' + value + '</div>');
+			}
+			else {
+				$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
+			}
+		});
+    // ceb
+    $('#currentRoom').empty();
+    $('#currentRoom').append('Current room: ');
+    $('#currentRoom').append(current_room);
+	});
+
+
+
+});
